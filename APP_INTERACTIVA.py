e8307e6cc41df8f816f23f980d5a131180ab4365
1902797c <1902797c@umich.mx>
2026-05-06 18:52:27 -0600

    Update APP_INTERACTIVA.py

--- APP_INTERACTIVA.py
+++ APP_INTERACTIVA.py
@@ -289,7 +289,7 @@ if st.sidebar.button("Generar curva"):
         "Confinado_est": fc_conf_est,
         "Confinado_din": fc_conf_din})
     
-    st.dataframe(df, use_container_width=True)
+    st.dataframe(df.head(5))
     st.download_button(
         "📥 Descargar resultados (CSV)",
         df.to_csv(index=False),
